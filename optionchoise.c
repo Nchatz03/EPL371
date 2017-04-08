5c550d87bcfe81e9d52c9c6796c48d92d920fa0a
Nchatz03 <chatzigiannisnick@gmail.com>
2017-04-08 20:00:45 +0300

    Add files via upload

---
+++ optionchoise.c
@@ -0,0 +1,52 @@
+char * requestOption;
+
+
+if(strcmp(requestOption,"OPTIONS"){
+
+   notImplementedOption();
+
+}
+else if(strcmp(requestOption,"GET"){
+
+   getOption();
+
+}
+else if(strcmp(requestOption,"HEAD"){
+
+   headOption();
+
+}
+else if(strcmp(requestOption,"POST"){
+
+   notImplementedOption();
+
+}
+else if(strcmp(requestOption,"PUT"){
+
+   notImplementedOption();
+
+}
+else if(strcmp(requestOption,"DELETE"){
+
+   deleteOption();
+
+}
+else if(strcmp(requestOption,"TRACE"){
+
+   notImplementedOption();
+
+}
+else if(strcmp(requestOption,"CONNECT"){
+
+   notImplementedOption();
+
+}else if(strcmp(requestOption,"end"){
+
+   exit(0);
+
+else{
+
+   notAnOption();
+}
+
+
