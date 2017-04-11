03bf71aec83f7995876f070b45b9eb2dbf7e23fd
Nchatz03 <chatzigiannisnick@gmail.com>
2017-04-11 22:25:59 +0300

    Final commit
    Comments added

--- components.h
+++ components.h
@@ -1,8 +1,34 @@
 #ifndef _COMPONENTS_H
 #define _COMPONENTS_H
 
-void defineRequest(char * stream,char* requestOption,char * connection,char * contenttype,char  * requestitem);
+/** @brief Define the request         
+*
+* This function implements the definition of a request send from the 
+* client to the server in order to reply. If the request option is not implemented
+* server stream the default message 501 if the option is not exist or is wrong 
+* server stream the message 406. if end is given communication close.
+* 
+* @param char* stream - the output stream
+* @param char* requestoption - the option client request
+* @param char* connection - the connection status
+* @param char* contenttype - the type of the requested item
+* @param char* requestitem - the path of the requested item
+* @param char* body - the file body stream
+*
+* @return int - if request is get to multistream .
+*/
+int defineRequest(char* stream, char* requestoption, char* connection, char* contenttype, char* requestitem, char* body);
 
-void defineContentType( char* extension ,char** type);
+/** @brief Define the requestet item type        
+*
+* This function implements the definition of what type is the requested item type
+* that client request 
+* 
+* @param char* extension - the file extension
+* @param char** type - the request item type
+*
+* @return void.
+*/
+void defineContentType(char* extension , char** type);
 
 #endif
